8f8deb903915338dc5d9cbe9c46fb8e7fe26a63f
Aryan Tyagi 27 <mraryanttyagiji@gmail.com>
2025-06-08 03:33:19 +0530

    Corrected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// // Pages
+// import Home from './components/Home';
+// import ParentSignup from './pages/ParentSignup';
+// import ParentLogin from './pages/ParentLogin';
+// import Admin from './pages/Admin';
+// import Dashboard from './components/Dashboard';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/signup" element={<ParentSignup />} />
+//         <Route path="/login" element={<ParentLogin />} />
+//         <Route path="/admin" element={<Admin />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -5,7 +31,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ParentSignup from './pages/ParentSignup';
 import ParentLogin from './pages/ParentLogin';
-import Admin from './pages/Admin';
+import DashboardAdmin from './pages/dashboardAdmin';
 import Dashboard from './components/Dashboard';
 
 function App() {
@@ -15,7 +41,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<ParentSignup />} />
         <Route path="/login" element={<ParentLogin />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin" element={<DashboardAdmin />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
@@ -23,3 +49,5 @@ function App() {
 }
 
 export default App;
+
+// export default App;
