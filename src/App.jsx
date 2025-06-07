ff27894f9547b169767e9a18465405f871539d8f
Aryan-Tyagi-27 <mraryantyagiji@gmail.com>
2025-06-08 03:17:51 +0530

    Complete Admin Dashboard with Statistics and Data Visualization

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,3 @@
-// import React, { useState } from 'react'
-// import "./App.css"
-// import TodoInput from './components/ToDoInput'
-// import Todolist from './components/ToDoList';
-// function App() {
-//   const [listTodo,setListTodo]=useState([]);
-//   let addList = (inputText)=>{
-//     if(inputText!=='')
-//       setListTodo([...listTodo,inputText]);
-//   }
-//   const deleteListItem = (key)=>{
-//     let newListTodo = [...listTodo];
-//     newListTodo.splice(key,1)
-//     setListTodo([...newListTodo])
-//   }
-//   return (
-//     <div className="main-container">
-//       <div className="center-container">
-//         <TodoInput addList={addList}/>
-//         <h1 className="app-heading">TODO</h1>
-//         <hr/>
-//         {listTodo.map((listItem,i)=>{
-//           return (
-//             <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
-//           )
-//         })}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
-
-// Shri's Todo App
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -40,21 +5,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ParentSignup from './pages/ParentSignup';
 import ParentLogin from './pages/ParentLogin';
-// import AdminLogin from './pages/AdminLogin'; // Optional
-
-// Components
-// import Navbar from './components/Navbar'; // If you want a global navbar
-// import Layout from './components/Layout'; // If you're using a layout wrapper
+import Admin from './pages/Admin';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Router>
-      {/* Optional: Wrap routes with <Layout> if needed */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<ParentSignup />} />
-        {/* <Route path="/admin" element={<Admin />} /> */}
         <Route path="/login" element={<ParentLogin />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
